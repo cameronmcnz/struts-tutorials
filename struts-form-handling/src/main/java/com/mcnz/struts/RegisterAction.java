c247d6a6ed7bd6cc2dc2476a1123b79397e19eab
Syster Sally <portal@example.com>
2020-09-11 08:08:10 -0400

    Add Validation Functionality

--- struts-form-handling/src/main/java/com/mcnz/struts/RegisterAction.java
+++ struts-form-handling/src/main/java/com/mcnz/struts/RegisterAction.java
@@ -2,11 +2,15 @@ package com.mcnz.struts;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 @Action("/register")
-@Result(name = "success", location = "/thankyou.jsp")
+@Results({
+	@Result(name = "success", location = "/thankyou.jsp"),
+	@Result(name = "input", location = "/register.jsp")
+})
 public class RegisterAction extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
@@ -15,6 +19,19 @@ public class RegisterAction extends ActionSupport {
 		System.out.println("The age is: " + personBean.getAge());
 		return SUCCESS;
 		//return "success";
+	}
+	
+	public void validate() {
+	    if (personBean.getFirstName().length() == 0) {
+	        addFieldError("personBean.firstName", "First name is required.");
+	    }
+	    if (personBean.getEmail().length() == 0) {
+	        addFieldError("personBean.email", "Email is required.");
+	    }
+	    if (personBean.getAge() < 18) {
+	        addFieldError("personBean.age", "Age is required and must be 18 or older");
+	    }
+		
 	}
 	
 	private Person personBean;
--- struts-form-handling/src/main/webapp/register.jsp
+++ struts-form-handling/src/main/webapp/register.jsp
@@ -1,6 +1,9 @@
 <%@ taglib prefix="s" uri="/struts-tags" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
+<head>
+<s:head />
+</head>
 <body>
 
 
